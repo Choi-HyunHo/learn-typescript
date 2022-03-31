b2388cad35577349c1ff5da5dcef5de8c0f909b9
com <x7788@naver.com>
2022-03-31 16:45:14 +0900

    ♻️ 타입별칭 및 인터페이스

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,10 +1,23 @@
 // any 타입 : string, number, array 등 모든 타입을 통틀어서 본다.
 // void : 반환값이 없다.(리턴값들이 없는 경우)
+// 중복된 코드 제거(타입 별칭)
+//type Todo = {
+//  id: number;
+// title: string;
+//  done: boolean;
+//};
 
-let todoItems: object[]; // object로 이루어진 배열이다.
+// 인터페이스
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+};
+
+let todoItems: Todo[]; // object로 이루어진 배열이다.
 
 // api
-function fetchTodoItems(): object[] {
+function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -19,7 +32,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: { id: number; title: string; done: boolean }): void {
   todoItems.push(todo);
 }
 
@@ -27,7 +40,10 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: object): void {
+function completeTodo(
+  index: number,
+  todo: { id: number; title: string; done: boolean }
+): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -42,8 +58,19 @@ function showCompleted(): object[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = {
+    id: 4,
+    title: '아이템 4',
+    dome: false,
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: '아이템 5',
+    done: false,
+  });
 }
 
 // NOTE: 유틸 함수
