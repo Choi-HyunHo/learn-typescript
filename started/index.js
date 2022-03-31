71459b9e764a32a3a6e6756a2d6ea76a8a0883f9
com <x7788@naver.com>
2022-03-31 14:19:30 +0900

    ✨ 초기 설정

---
+++ started/index.js
@@ -0,0 +1,4 @@
+function sum(a, b, c) {
+    return a + b + c;
+}
+sum(10, 20, 30);
---
+++ started/index.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number, c:number) : number {
+    return a + b + c;
+}
+
+sum(10, 20, 30);
