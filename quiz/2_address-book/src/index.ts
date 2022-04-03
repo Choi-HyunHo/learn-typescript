0c06473b730e14e9347c2a9697360bbe49fa5f46
com <x7788@naver.com>
2022-04-03 18:37:14 +0900

    📝 모듈화 실습

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,20 +1,5 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneType {
-  Home = 'home',
-  Office = 'office',
-  Studio = 'studio',
-}
+import {Contact} from './a';
+import {PhoneType} from './a';
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
